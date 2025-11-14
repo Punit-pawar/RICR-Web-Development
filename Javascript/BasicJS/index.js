ddd47876233ecc7947ec0d19f593c9055ac8e21b
Punit-pawar <punitpawar59@gmail.com>
2025-11-14 20:27:36 +0530

    basic Javascript

---
+++ Javascript/BasicJS/index.js
@@ -0,0 +1,25 @@
+var a = 10;
+
+var a = 20;
+
+let b = 10;
+
+console.log(a, b);
+
+let ar = ["abc", 1234, 56.77, "a", true];
+console.log(ar);
+
+alert("this is alart");
+
+console.warn("this is warning");
+
+console.error("this is error");
+
+if (confirm("are you 18+")) 
+    {
+    console.log("thanks");
+} 
+else 
+    {
+     console.error("it's ok");
+}
