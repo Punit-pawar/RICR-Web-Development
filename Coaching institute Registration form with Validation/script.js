1bdf26ae0fb85c0ab2a566f6cd67acf993c50303
Punit-pawar <punitpawar59@gmail.com>
2025-11-21 20:21:35 +0530

    Javascript

---
+++ Coaching institute Registration form with Validation/script.js
@@ -0,0 +1,39 @@
+function submit(){
+
+    console.log("submit Button Clicked")
+
+    const fullname = document.getElementById("fullname").value;
+    console.log(fullname)
+    document.getElementById("fullname").value = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let seletedbatchtiming=[];
+
+
+    document
+    const batch = document.querySelectorAll("input[name='batch']:checked");
+    batch.forEach((element)=>{
+        seletedbatchtiming.push(element.value)
+
+    })
+
+    console.log(seletedbatchtiming);
+    
+}
