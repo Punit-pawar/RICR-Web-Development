e492ebadf734e869d7cdf0f05ae24e435b1b9480
Punit-pawar <punitpawar59@gmail.com>
2025-12-07 13:19:54 +0530

    loaction map game

---
+++ map game/script.js
@@ -0,0 +1,9 @@
+function state(){
+
+    console.log("search Button Clicked")
+
+    const state = document.getElementById("state").value;
+    console.log(state)
+    document.getElementById("state").value = "";
+
+}
