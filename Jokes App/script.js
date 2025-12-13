a60148ca87b54a0221eaf18f4460eb2e73d96312
Punit-pawar <punitpawar59@gmail.com>
2025-12-13 20:26:39 +0530

    joke app

---
+++ Jokes App/script.js
@@ -0,0 +1,10 @@
+async function getnewjoke(){
+
+    const response = await fetch("https://official-joke-api.appspot.com/jokes/random");
+
+    const data = await response.json();
+
+    document.getElementById("setup").innerText = data.setup;
+    document.getElementById("punchline").innerText = data.punchline;
+
+}
