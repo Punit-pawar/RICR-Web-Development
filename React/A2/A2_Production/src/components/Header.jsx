465ec494bbcd7c719fec26bb5569938fd68b43c5
Punit-pawar <punitpawar59@gmail.com>
2026-01-03 23:48:12 +0530

    update

---
+++ React/A2/A2_Production/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className="bg-orange-200 px-4 py-2 h-30 text-6xl text-black text-center flex justify-center items-center gap-4">
+        <span className="font-bold">A2 Production & Events</span>
+    </div>
+    </>
+  )
+}
+
+export default Header;
