88321db463ceede2870517ba038730a1e2a0089e
Punit-pawar <punitpawar59@gmail.com>
2026-01-04 19:29:25 +0530

    update

--- React/Registration/src/components/Header.jsx
+++ React/Registration/src/components/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <>
 
     <div className="flex justify-between bg-amber-200 items-center h-15">   
-        <div className="text-4xl fw-bold italic ms-5 cursor-pointer text-fuchsia-500">
+        <div className="text-4xl fw-bold italic ms-5 cursor-pointer text-fuchsia-400">
           Glow More
         </div>
     </div>
