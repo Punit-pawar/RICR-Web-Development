c4c36a524b00cfa5e34c2d0e019ce4c1455893f6
Punit-pawar <punitpawar59@gmail.com>
2026-01-11 19:15:56 +0530

    update

--- Dinex food/client/src/App.jsx
+++ Dinex food/client/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Register from "./pages/Register"
 
 const App = () => {
   return (
